d7a0bd853d938e72b996f877aeabc6c1274c9276
Soundharyaam <soundharyabharatiya@gmail.com>
2019-06-09 17:02:09 +0530

    Updates

---
+++ precache-manifest.e5ff69ae552a5adca1034896b6c69833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8dfba7ea315b7b668ff",
+    "url": "/Movie-analysis-app/static/js/runtime~main.c8dfba7e.js"
+  },
+  {
+    "revision": "7f8528790742de16d78c",
+    "url": "/Movie-analysis-app/static/js/main.7f852879.chunk.js"
+  },
+  {
+    "revision": "2665c4a6335b6cf205f2",
+    "url": "/Movie-analysis-app/static/js/1.2665c4a6.chunk.js"
+  },
+  {
+    "revision": "7f8528790742de16d78c",
+    "url": "/Movie-analysis-app/static/css/main.381a7545.chunk.css"
+  },
+  {
+    "revision": "2665c4a6335b6cf205f2",
+    "url": "/Movie-analysis-app/static/css/1.eda60130.chunk.css"
+  },
+  {
+    "revision": "dc9efef3b0b46d7fa8b5c01daa8adfe4",
+    "url": "/Movie-analysis-app/index.html"
+  }
+];
